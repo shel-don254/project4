04203e8169be03ff8abfe07c857539be302c53be
sheldon <sheldonmwangi254@gmail.com>
2023-10-05 16:43:38 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,15 +1,91 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db, User, Pizza, Topping, Order, pizza_toppings
+from models import db, User, Pizza, Topping, Order
+
 app = Flask(__name__)
 
 # Configuring the app
-app.config['SECRET_KEY'] = '19902022' 
+app.config['SECRET_KEY'] = '19902022'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# Routes for listing all users
+@app.route('/users', methods=['GET'])
+def list_users():
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+    return jsonify(serialized_users)
 
+# Route for creating a new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Username and password are required'}), 400
+
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+# Routes for listing all pizzas and creating a new pizza
+@app.route('/pizzas', methods=['GET', 'POST'])
+def pizzas():
+    if request.method == 'GET':
+        pizzas = Pizza.query.all()
+        serialized_pizzas = [pizza.serialize() for pizza in pizzas]
+        return jsonify(serialized_pizzas)
+    elif request.method == 'POST':
+        data = request.get_json()
+        name = data.get('name')
+        price = data.get('price')
+
+        if not name or not price:
+            return jsonify({'message': 'Name and price are required'}), 400
+
+        pizza = Pizza(name=name, price=price)
+        db.session.add(pizza)
+        db.session.commit()
+
+        return jsonify({'message': 'Pizza created successfully'}), 201
+
+# Route for listing all toppings
+@app.route('/toppings', methods=['GET'])
+def list_toppings():
+    toppings = Topping.query.all()
+    serialized_toppings = [topping.serialize() for topping in toppings]
+    return jsonify(serialized_toppings)
+
+# Route for creating a new order
+@app.route('/orders', methods=['POST'])
+def create_order():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    pizza_id = data.get('pizza_id')
+    quantity = data.get('quantity')
+
+    if not user_id or not pizza_id or not quantity:
+        return jsonify({'message': 'User ID, pizza ID, and quantity are required'}), 400
+
+    user = User.query.get(user_id)
+    pizza = Pizza.query.get(pizza_id)
+
+    if not user or not pizza:
+        return jsonify({'message': 'User or pizza not found'}), 404
+
+    order = Order(user=user, pizza=pizza, quantity=quantity)
+    db.session.add(order)
+    db.session.commit()
+
+    return jsonify({'message': 'Order created successfully'}), 201
+
+if __name__ == '__main__':
+    app.run()
--- server/models.py
+++ server/models.py
@@ -1,13 +1,18 @@
-
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy() 
+db = SQLAlchemy()
 
+# many-to-many association table
+pizza_toppings = db.Table(
+    'pizza_toppings',
+    db.Column('pizza_id', db.Integer, db.ForeignKey('pizzas.id'), primary_key=True),
+    db.Column('topping_id', db.Integer, db.ForeignKey('toppings.id'), primary_key=True)
+)
+
+# User Model
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-
-    serialize_rules = ('-orders.user')
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -18,52 +23,71 @@ class User(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<User {self.username}>'
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+        }
+
+# Pizza Model
 class Pizza(db.Model, SerializerMixin):
     __tablename__ = 'pizzas'
 
-    serialize_rules = ('-toppings.pizzas',)
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
     price = db.Column(db.Float, nullable=False)
 
-    toppings = db.relationship('Topping', secondary='pizza_toppings', back_populates='pizzas', lazy=True)
+    toppings = db.relationship('Topping', secondary=pizza_toppings, back_populates='pizzas', lazy=True)
+    orders = db.relationship('Order', back_populates='pizza', lazy=True)
 
     def __repr__(self):
         return f'<Pizza {self.name}>'
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+        }
+
+#Topping Model
 class Topping(db.Model, SerializerMixin):
     __tablename__ = 'toppings'
-    serialize_rules = None
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False, unique=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
 
-    pizzas = db.relationship('Pizza', secondary='pizza_toppings', back_populates='toppings', lazy=True)
-
+#many-to-many relationship with pizzas
+    pizzas = db.relationship('Pizza', secondary=pizza_toppings, back_populates='toppings', lazy=True)
 
     def __repr__(self):
         return f'<Topping {self.name}>'
 
-pizza_toppings = db.Table('pizza_toppings',
-    db.Column('pizza_id', db.Integer, db.ForeignKey('pizzas.id'), primary_key=True),
-    db.Column('topping_id', db.Integer, db.ForeignKey('toppings.id'), primary_key=True)
-)
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
 
-
-
+# Define Order Model
 class Order(db.Model, SerializerMixin):
     __tablename__ = 'orders'
 
-    serialize_rules = ('-user.orders',)
-
     id = db.Column(db.Integer, primary_key=True)
-    order_date = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    total_amount = db.Column(db.Float, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
-    user = db.relationship('User', back_populates='orders', lazy=True)
+    user = db.relationship('User', back_populates='orders')
+    pizza = db.relationship('Pizza', back_populates='orders')
 
     def __repr__(self):
         return f'<Order {self.id}>'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'pizza_id': self.pizza_id,
+            'quantity': self.quantity,
+        }
